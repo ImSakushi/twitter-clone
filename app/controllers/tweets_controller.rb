71812b9ed455ee1d950d0f5628a45d7e6e0640ea
ImSakushi <pablo.casola@mmibordeaux.com>
2023-03-01 17:04:59 +0100

    initial commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,87 @@
+class TweetsController < ApplicationController
+  before_action :set_tweet, only: %i[ show edit update destroy ]
+
+  require 'open-uri'
+  require 'json'
+
+  # GET /tweets or /tweets.json
+  def index
+    @tweets = Tweet.all
+  end
+
+
+  # GET /tweets/1 or /tweets/1.json
+  def show
+  end
+
+  # GET /tweets/new
+  def new
+    @tweet = Tweet.new
+  end
+
+  # GET /tweets/1/edit
+  def edit
+  end
+
+  def get_joke
+    url = 'https://blague.xyz/api/joke/random'
+    joke = JSON.parse(open(url).read)
+    @joke = joke['joke']
+    render json: { joke: @joke }
+  end
+
+  def random_joke
+    response = get_joke
+    @joke = response[:joke]
+  end
+
+  # POST /tweets or /tweets.json
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to tweets_path(@tweet), notice: "Le tweet a été créé." }
+        format.json { render :show, status: :created, location: @tweet }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+  # PATCH/PUT /tweets/1 or /tweets/1.json
+  def update
+    respond_to do |format|
+      if @tweet.update(tweet_params)
+        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully updated." }
+        format.json { render :show, status: :ok, location: @tweet }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @tweet.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tweets/1 or /tweets/1.json
+  def destroy
+    @tweet.destroy
+
+    respond_to do |format|
+      format.html { redirect_to tweets_url, notice: "Tweet was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def tweet_params
+      params.require(:tweet).permit(:username, :body)
+    end
+end
